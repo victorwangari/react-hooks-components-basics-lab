6f86bf3effe6367f44b327a8cfbf3dec3e2083c4
Victor kamau <Victor Wangari>
2024-04-23 11:43:56 +0300

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,4 @@
+import { Hook } from "mocha";
 import React from "react";
 
 function NavBar() {
@@ -16,15 +17,21 @@ function Home() {
   );
 }
 
-{/* write an <About> component here */}
+function About(){
+  return(
+    <div id="about">
+      <h2>About</h2>
+    </div>
+  )
+}
 
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home/>
+      <About/>
     </div>
   );
 }
